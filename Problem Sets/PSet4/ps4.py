04201e9cac2338a75df8915f2c6894532b0b4262
vit-aborigen <vit.aborigen@gmail.com>
2017-04-27 18:02:19 +0300

    PSET 4 done

--- Problem Sets/PSet4/ps4.py
+++ Problem Sets/PSet4/ps4.py
@@ -196,12 +196,12 @@ def evaluate_models_on_training(x, y, models):
 raw_data = Climate('data.csv')
 
 # Problem 3
-# y = []
-# x = INTERVAL_1
-# for year in INTERVAL_1:
-#     y.append(raw_data.get_daily_temp('BOSTON', 1, 10, year))
-# models = generate_models(x, y, [1])
-# evaluate_models_on_training(x, y, models)
+y = []
+x = INTERVAL_1
+for year in INTERVAL_1:
+    y.append(raw_data.get_daily_temp('BOSTON', 1, 10, year))
+models = generate_models(x, y, [1])
+evaluate_models_on_training(x, y, models)
 
 
 # Problem 4: FILL IN MISSING CODE TO GENERATE y VALUES
